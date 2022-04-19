b23cee3fade48a8ead4957092f38387b88c062f5
Felipe Marques <felipe.marques.br@avenuecode.com>
2022-04-19 14:30:19 -0300

    initial tests

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,19 @@
+const { expect } = require("@jest/globals");
+const expectExport = require("expect");
+
 describe("Gilded Rose", function() {
 
   it("should do something", function() {
-    update_quality();
+    // prepare
+    const items = [
+      new Item('Conjured Mana Cake', 3, 6)
+    ];
+
+    // act
+    const itemsResult = update_quality(items);
+
+    // assert
+    expect(itemsResult).toBe([new Item('Conjured Mana Cake', 3, 4)]);
   });
 
 });
