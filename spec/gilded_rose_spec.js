86b5690edb7fc1022d6ec17346b96257e852e0e6
Felipe Marques <felipe.marques.br@avenuecode.com>
2022-04-19 22:58:46 -0300

    refactor all update_quality method

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,8 +1,25 @@
 const { expect } = require("@jest/globals");
 const expectExport = require("expect");
-const { Item, update_quality} = require("../src/gilded_rose");
+const { Item, items, update_quality} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
+
+  it("Test all items", function() {
+    // prepare
+
+    // act
+    const itemsResult = update_quality(items);
+
+    // assert
+    expect(itemsResult).toStrictEqual([
+      new Item('+5 Dexterity Vest', 9, 19),
+      new Item('Aged Brie', 1, 2),
+      new Item('Elixir of the Mongoose', 4, 6),
+      new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+      new Item('Backstage passes to a TAFKAL80ETC concert', 14, 19),
+      new Item('Conjured Mana Cake', 2, 4),
+    ]);
+  });
 
   it("Test conjured quality decrease", function() {
     // prepare
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -13,59 +13,106 @@ items.push(new Item('Sulfuras, Hand of Ragnaros', 0, 80));
 items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
-function update_quality(items) {
-  for (var i = 0; i < items.length; i++) {
+function checkItemProperties(item) {
+  if (!item.hasOwnProperty('quality') || !item.hasOwnProperty('sell_in')) {
+    return false;
+  }
 
-    if (items[i].name == 'Conjured Mana Cake') {
-        items[i].quality = items[i].quality - 2; 
-        items[i].sell_in = items[i].sell_in - 1; 
-        continue;
-    }
+  return true;
+}
 
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
+function checkQualityValue(item) {
+
+  const itemsByPass = [
+    'Sulfuras, Hand of Ragnaros'
+  ];
+
+  if (itemsByPass.includes(item.name)) {
+    return true;
+  }
+
+  if (item.quality >= 0 && item.quality <= 50) {
+    return true;
+  }
+
+  return false;
+}
+
+function updateConjuredItem(item) {
+
+  if (item.name.match(/Conjured/)) {
+    item.quality -= 1;
+  }
+
+  return item;
+}
+
+function updateSulfurasItem(item) {
+
+  // because for all items the quality and sell_in are updated in the beginning
+  if (item.name.match(/Sulfuras/)) {
+    item.sell_in += 1;
+    item.quality += 1;
+  }
+
+  return item;
+}
+
+function updateItemThatGainQualityWhenDecreaseSellIn(item) {
+  if (item.name.match(/(Aged Brie|Backstage passes)/gi)) {
+    if (item.sell_in <= 10) {
+      if (item.sell_in <= 5) {
+        item.quality += 3;
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
+        item.quality += 2;
       }
     }
   }
+  return item;
+}
+
+function updateQualityIfSellInLessThanZero(item) {
+  if (item.sell_in < 0) {
+    item.quality -= 1;
+  }
+  return item;
+}
+
+function update_quality(items) {
+
+  // constraints ...
+  // v - all items have sell_in and quality
+  // v - quality never is less than 0
+  // v - quality never is more than 50
+  // v - quality is >= 0
+  // v - quality is <= 50
+  // - Sulfuras is legendary item not decrease quality and sell_in, your quality always 80 and never change
+  // - "Backstage passes", like aged brie, increases in *quality* as it's *sell_in* value decreases; *quality* 
+  //      increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but *quality* 
+  //      drops to 0 after the concert
+  // - Once the *sell_in* days is less than zero, *quality* degrades twice as fast
+
+  items.map((item) => {
+
+    if (!checkItemProperties(item)) {
+      throw new Error('invalid properties, needs quality and sell_in');
+    }
+
+    if (!checkQualityValue(item)) {
+      throw new Error(`invalid quality value: ${item.quality}`);
+    }
+
+    // for all items
+    item.sell_in -= 1;
+    item.quality -= 1;
+
+    item = updateConjuredItem(item);
+    item = updateSulfurasItem(item);
+    item = updateItemThatGainQualityWhenDecreaseSellIn(item);
+    item = updateQualityIfSellInLessThanZero(item);
+
+    return item;
+  });
 
   return items;
 }
@@ -73,5 +120,10 @@ function update_quality(items) {
 module.exports = {
   Item,
   items,
-  update_quality,
+  checkItemProperties,
+  checkQualityValue,
+  updateConjuredItem,
+  updateItemThatGainQualityWhenDecreaseSellIn,
+  updateQualityIfSellInLessThanZero,
+  update_quality
 }
